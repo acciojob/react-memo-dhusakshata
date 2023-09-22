9c7d6d81915ad444a39270cdca6850f07a03b8f7
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-09-22 18:12:38 +0530

    submitting code-Fri Sep 22 2023 18:12:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,56 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from 'react';
 
+function Memo() {
+  // Step 1: Initialize state for todos, count, and memo input
+  const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(0);
+  const [memoInput, setMemoInput] = useState('');
+
+  // Step 2: Create a function to add a new todo
+  const addTodo = () => {
+    setTodos([...todos, 'New todo']);
+  };
+
+  // Step 3: Create a function to increment the count
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  // Step 4: Create a function to handle memo input and add items
+  const handleMemoInput = (e) => {
+    setMemoInput(e.target.value);
+  };
+
+  const addMemoItem = () => {
+    if (memoInput.length > 5) {
+      setTodos([...todos, memoInput]);
+      setMemoInput('');
+    } else {
+      alert('Text should be greater than 5 characters');
+    }
+  };
+
+  // Step 5: Render the components with the defined functionality
+  return (
+    <div>
+      <h1>React Memo</h1>
+      <button onClick={addTodo}>Add Todo</button>
+      <button onClick={incrementCount}>Increment Count</button>
+      <p>Count: {count}</p>
+      <input
+        type="text"
+        placeholder="Enter memo item"
+        value={memoInput}
+        onChange={handleMemoInput}
+      />
+      <button onClick={addMemoItem}>Submit</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Memo;
