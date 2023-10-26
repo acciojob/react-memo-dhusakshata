0e4c1f9fec481cdf77129ed1832e3bc82ec14366
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-10-26 11:58:07 +0530

    new todomemo

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,20 @@
-import React, { useState, useEffect } from 'react';
-
-function Memo() {
-  // Step 1: Initialize state for todos, count, and memo input
-  const [todos, setTodos] = useState([]);
-  const [count, setCount] = useState(0);
-  const [memoInput, setMemoInput] = useState('');
-
-  // Step 2: Create a function to add a new todo
-  const addTodo = () => {
-    setTodos([...todos, 'New todo']);
-  };
-
-  // Step 3: Create a function to increment the count
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  // Step 4: Create a function to handle memo input and add items
-  const handleMemoInput = (e) => {
-    setMemoInput(e.target.value);
-  };
-
-  const addMemoItem = () => {
-    if (memoInput.length > 5) {
-      setTodos([...todos, memoInput]);
-      setMemoInput('');
-    } else {
-      alert('Text should be greater than 5 characters');
-    }
-  };
-
-  // Step 5: Render the components with the defined functionality
-  return (
-    <div>
-      <h1>React Memo</h1>
-      <button onClick={addTodo}>Add Todo</button>
-      <button onClick={incrementCount}>Increment Count</button>
-      <p>Count: {count}</p>
-      <input
-        type="text"
-        placeholder="Enter memo item"
-        value={memoInput}
-        onChange={handleMemoInput}
-      />
-      <button onClick={addMemoItem}>Submit</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-    </div>
-  );
+App.js
+ 
+import React from "react";
+import ToDo from "./ToDo";
+import Counter from "./Counter";
+import Memo from "./Memo";
+ 
+const App = () =>{
+    return(
+       <div id="main">
+           <ToDo/>
+           <Counter/>
+           <Memo/>
+       </div>
+    )
 }
-
-export default Memo;
+ 
+export default App;
+ 
+ 
---
+++ src/components/Memo.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+ 
+const Memo = () =>{
+    const[text,setText] = useState("");
+    const[memo,setMemo] = useState([]);
+ 
+   function submitMemo(){
+    if(text.length>5)
+    {
+        setMemo([...memo,text]);
+        setText("");
+    }
+    else{
+        setText("");
+    }
+      
+   }
+ 
+    return(
+       <div>
+           <h3>Memo</h3>
+           <input id="skill-input" onChange={e=>setText(e.target.value)} value={text} />
+           <button id="skill-btn" onClick={submitMemo}>Submit</button>
+           <ul>
+            {
+                memo.map((ele)=>(
+                    <li id={`item-jumbotron`}>{ele}</li>
+                ))
+            }
+           </ul>
+       </div>
+    )
+}
+ 
+export default Memo;
