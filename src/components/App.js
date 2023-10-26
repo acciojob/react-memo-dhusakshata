e9a90df7a255fb641c24062e0c3fb2c709722acc
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-10-26 12:12:13 +0530

    again todomemo

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
-
- 
 import React from "react";
 import ToDo from "./ToDo";
 import Counter from "./Counter";
 import Memo from "./Memo";
- 
-const App = () =>{
-    return(
-       <div id="main">
-           <ToDo/>
-           <Counter/>
-           <Memo/>
-       </div>
-    )
-}
- 
+
+const App = () => {
+    return (
+        <div id="main">
+            <ToDo />
+            <Counter />
+            <Memo />
+        </div>
+    );
+};
+
 export default App;
- 
- 
--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,35 +1,28 @@
 import React, { useState } from "react";
- 
-const Memo = () =>{
-    const[text,setText] = useState("");
-    const[memo,setMemo] = useState([]);
- 
-   function submitMemo(){
-    if(text.length>5)
-    {
-        setMemo([...memo,text]);
-        setText("");
+
+const Memo = () => {
+    const [text, setText] = useState("");
+    const [memo, setMemo] = useState([]);
+
+    function submitMemo() {
+        if (text.length > 0) {
+            setMemo([...memo, text]);
+            setText("");
+        }
     }
-    else{
-        setText("");
-    }
-      
-   }
- 
-    return(
-       <div>
-           <h3>Memo</h3>
-           <input id="skill-input" onChange={e=>setText(e.target.value)} value={text} />
-           <button id="skill-btn" onClick={submitMemo}>Submit</button>
-           <ul>
-            {
-                memo.map((ele)=>(
-                    <li id={`item-jumbotron`}>{ele}</li>
-                ))
-            }
-           </ul>
-       </div>
-    )
-}
- 
+
+    return (
+        <div>
+            <h3>Memo</h3>
+            <input id="skill-input" onChange={e => setText(e.target.value)} value={text} />
+            <button id="skill-btn" onClick={submitMemo}>Submit</button>
+            <ul>
+                {memo.map((ele, index) => (
+                    <li key={`item-${index}`}>{ele}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
 export default Memo;
