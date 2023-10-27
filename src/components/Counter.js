168c220a39d48cc078afc40f202c4e21dfd20dac
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-10-27 12:22:10 +0530

    memo again

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-
-const Counter = () => {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div id="incr-cnt">
-            Count: {count}
-            <button id="incr-btn" onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-    );
-};
-
+ 
+const Counter = () =>{
+ 
+    const[count,setCount] = useState(1000000000);
+ 
+    return(
+      <div id="incr-cnt">
+      Count : 0<button id="incr-btn" onClick={()=>(setCount(count+1))}>New Todo</button>
+      <div id="calc">{count}</div>
+     </div>
+    )
+}
+ 
 export default Counter;
+ 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
-
-const ToDo = () => {
-    const [todo, setTodo] = useState([]);
-
-    return (
-        <div>
-            <h1>My ToDos</h1>
-            <ul>
-                {todo.map((ele, idx) => (
-                    <li key={`todo-${idx}`}>{ele}</li>
-                ))}
-            </ul>
-            <button id="add-todo-btn" onClick={() => setTodo([...todo, "New Todo"])}>
-                Add Todo
-            </button>
-        </div>
-    );
-};
-
+ 
+const ToDo = () =>{
+    const[todo,setTodo] = useState([]);
+  
+    return(
+       <div>
+           <h1>My ToDos</h1>
+           <ul>
+            {
+                todo.map((ele,idx)=>(
+                    <li id={`todo-${idx}`}>{ele}</li>
+                ))
+            }
+           </ul>
+           <button id="add-todo-btn" onClick={()=>setTodo([...todo,"New Todo"])}>Add Todo</button>
+       </div>
+    )
+}
+ 
 export default ToDo;
